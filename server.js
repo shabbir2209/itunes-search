3f2e8797a3b2a5c58758c5d75d7fc2a4c2cff88e
Shabbir <s.dawoodjee92@gmail.com>
2021-05-31 21:25:13 +0200

    changes

--- server.js
+++ server.js
@@ -28,11 +28,9 @@ app.get('/search', async (req,res) => {
 });
 
 // production code
-if (process.env.NODE_ENV === 'production') {           
-    app.use(express.static('client/build'));
-  
-    app.get('/search', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.get('*',(req,res)=> {res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
     });
 }
 
@@ -43,8 +41,6 @@ app.use(express.json());
 app.use(helmet());
 
 // listening on port 3001
-let server = app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`);
 });
-
-module.exports = server;
